3a588a87fd8290accdcc9caf6662aa6652e1e239
jcblanc2 <jclaytonblanc@gmail.com>
2024-08-10 11:09:20 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Test from "./pages/Test";
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/test' element={<Test />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <div className="text-teal-950 text-center text-2xl">Home</div>
+    )
+}
+
+export default Home
