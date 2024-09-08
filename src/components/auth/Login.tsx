743a000892c5bfc247f1ee6c908c55d9e6b01248
itzmars <marsdoulens@proton.me>
2024-09-07 18:01:25 -0700

    add changes in auth

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import useLogin from '../../hooks/useLogin';
 
 
@@ -11,9 +11,15 @@ interface LoginProps {
 const Login: React.FC<LoginProps> = ({ isVisible, onClose }) => {
     const { email, password, error, setEmail, setPassword, handleLogin } = useLogin();
 
+    const [showPassword, setShowPassword] = useState(false);
+
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         handleLogin(onClose);
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
     };
 
 
@@ -51,14 +57,36 @@ const Login: React.FC<LoginProps> = ({ isVisible, onClose }) => {
                             onChange={(e) => setEmail(e.target.value)}
                             required
                         />
-                        <input
-                            type="password"
+
+                        <div className='relative'>                        <input
+                            type={showPassword ? "text" : "password"}
                             placeholder="Password"
                             className="p-2  rounded-xl border w-full"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
                         />
+                        <button
+                        type='button'
+                        onClick={togglePasswordVisibility}
+                        >
+                            {
+                                showPassword ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 text-gray-500 fill-current"
+                                    viewBox="0 0 16 16">
+                                    <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z" />
+                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z" />
+                                </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 text-gray-500 fill-current"
+                                    viewBox="0 0 16 16">
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
+                                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7" />
+                                </svg>
+                            }
+                             </button>
+                        </div>
+
+
                         <div className="flex justify-end gap-4 items-center">
                             <a href='#' className="text-indigo-600 hover:underline">Forgot password?</a>
 
